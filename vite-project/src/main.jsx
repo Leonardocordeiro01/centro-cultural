53cd8121111ff6db1ca2f1a6b490da520a43831f
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-04-04 18:33:54 -0300

    organizacao de paginas

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Home from "./containers/home";
+import MyGlobalStyles from "./styles/globalStyles";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>{/* <App /> */}</StrictMode>
+  <StrictMode>
+    <MyGlobalStyles />
+    <Home />
+  </StrictMode>
 );
---
+++ vite-project/src/styles/globalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const myGlobalStyles = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; 
+}
+
+p {
+    font-size: 20px;
+}`;
+
+export default myGlobalStyles;
