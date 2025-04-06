c12720ad5e4e47e3cfd79e87b75a7dfe1341646b
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-04-06 01:24:53 -0300

    comecei a fazer o menu do site

--- vite-project/src/containers/home/index.jsx
+++ vite-project/src/containers/home/index.jsx
@@ -1,15 +1,34 @@
+import { Nav } from "./styles";
+import { ContainerNav } from "./styles";
+import { BntNav } from "./styles";
 import { Title } from "./styles";
-import { Seciton } from "./styles";
+import { DivInicial } from "./styles";
+import { MetadaDaTela } from "./styles";
 import { SectionTexto } from "./styles";
 
 function Home() {
   return (
-    <Seciton>
-      <div>
-        <Title>Centro Cultural Ludmila Amalia Batova Arambasic</Title>
-      </div>
-      <div>{/* colocar imagem ou video da casa da cultura */}</div>
-    </Seciton>
+    <div>
+      <Nav>
+        <ContainerNav>
+          <BntNav>Home</BntNav>
+          <BntNav>Contact</BntNav>
+          <BntNav>About</BntNav>
+          <BntNav>FAQ</BntNav>
+        </ContainerNav>
+      </Nav>
+      <DivInicial>
+        <MetadaDaTela>
+          <Title>Centro Cultural Ludmila Amalia Batova Arambasic</Title>
+        </MetadaDaTela>
+        <MetadaDaTela>
+          <img
+            src="C:\Users\leona\OneDrive\Documentos\GitHub\centro cultural\vite-project\public\logo casa da cultura.png"
+            alt=""
+          />
+        </MetadaDaTela>
+      </DivInicial>
+    </div>
   );
 }
 
--- vite-project/src/containers/home/styles.js
+++ vite-project/src/containers/home/styles.js
@@ -1,16 +1,47 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-  color: white;
-  background: black;
+export const Nav = styled.div`
+  position: relative;
+  width: 400px;
+  height: 60px;
 `;
 
-export const Seciton = styled.section`
+export const ContainerNav = styled.div`
+  position: absolute;
+  inset: 0;
+  background: #bef6;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0.5em;
+`;
+
+export const BntNav = styled.div`
+  padding: 0.5em 1.5em;
+  color: #fff;
+  cursor: pointer;
+  transition: 0.1s;
+
+  &:hover {
+    background: #fff3;
+  }
+`;
+
+export const Title = styled.h1`
+  color: black;
+`;
+
+export const DivInicial = styled.div`
+  display: flex;
+  flex-direction: row;
   align-items: center;
   background: rgb(243, 212, 212);
   color: black;
+`;
+
+export const MetadaDaTela = styled.div`
+  width: 50%;
 `;
 
 export const SectionTexto = styled.section`
